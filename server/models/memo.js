9275d18f419c59c25c58f5c8ced3358de0a2293c
Eri Irawan <email": null>
2017-03-19 16:05:00 +0700

    add model memo:

---
+++ server/models/memo.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+require('../config/db')
+
+let memoSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'title is require']
+  },
+  content: {
+    type: String,
+    required: [true, 'content is require']
+  },
+  slug: String
+}, {
+  timestamps: true
+})
+
+let memo = mongoose.model('memo', memoSchema)
+
+module.exports = memo
